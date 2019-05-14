7b3a5825216899a51a9dd2fa2c71eb9b36eacb04
Eduardo <educastro100@gmail.com>
2019-05-13 23:02:07 -0300

    Adicionando estrutura do CodeIgniter e mais alguns arquivos.

---
+++ application/controllers/cLogin.php
@@ -0,0 +1,11 @@
+<?php 
+
+class CLogin extends CI_Controller{
+
+    function index(){
+        $this->load->view('loginInicio');
+    }
+
+}
+
+?>
---
+++ application/views/loginInicio.php
@@ -0,0 +1,101 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title> Teste do Eduardo de funcionalidades do CodeIgniter </title>
+
+
+    <!-- Importando o Materialize  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Importando os icones do Materialize -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!-- <div class="container  blue-grey">
+
+
+        <div class="container">
+        <div class="card-panel blue-grey">
+            <h1 class="center-align"> Library </h1>
+            <p class="center-align">Choose your login</p>
+            </div> 
+        </div>
+
+
+        <div class="row center-align">
+            <button class="hoverable waves-effect waves-light btn-large">
+                    <i class="material-icons" style="font-size:300%">build</i> 
+            </button>
+            
+            <button class="hoverable waves-effect waves-light btn-large">
+                    <i class="material-icons" style="font-size:300%">person</i> 
+            </button>
+
+            <button class="hoverable waves-effect waves-light btn-large">
+                    <i class="material-icons" style="font-size:300%">library_books</i> 
+            </button>
+
+        </div>
+
+
+        <footer class="page-footer footer-copyright cyan darken-4">
+
+
+            <div class="container">
+                © 2019 Doberman Studio
+                <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+
+        </footer>
+    </div> -->
+
+
+
+
+
+    <div class="card-panel lime lighten-5">
+        <h1 class="center-align">LIBRARY DOBERMAN</h1>
+        <h5 class="center-align">Think big, take a book</h5>
+
+        <div class="divider"></div>
+
+        <br><br><br>
+
+        <div class="row">
+
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        
+                        <span class="card-title">Card Title</span>
+                        <a class="btn-floating halfway-fab waves-effect waves-light red"><i
+                                class="material-icons">arrow_forward</i></a>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information. I am convenient
+                            because I require little markup to use effectively.</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+        </div>
+
+
+    </div>
+
+
+
+
+</body>
+
+<!-- Importando o Materialize  -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+</html>
